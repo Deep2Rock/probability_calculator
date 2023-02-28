f67b6e3762f42c07a5104e591076a54c32340f8b
Kiss Bertalan <kissbertalan07@gmail.com>
2023-03-01 00:45:07 +0100

    Quintent API is not working properly. er: 401, 403. Own server run

--- probability_calculation/index.js
+++ probability_calculation/index.js
@@ -190,15 +190,17 @@ function checking()
 }
 console.log("api should be added")
 
-const options = {
-	method: 'AAPL',
-	headers: {
-		'X-RapidAPI-Key': 'a605d354a4msh906c743e3a9eff5p1254c3jsnb4e62139467b',
-		'X-RapidAPI-Host': 'quotient.p.rapidapi.com'
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://quotient.p.rapidapi.com/equity/live?symbol=TSLA&timezone=UTC",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "a605d354a4msh906c743e3a9eff5p1254c3jsnb4e62139467b",
+		"X-RapidAPI-Host": "quotient.p.rapidapi.com"
 	}
 };
 
-fetch('https://quotient.p.rapidapi.com/equity/live?symbol=TSLA&timezone=UTC', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
